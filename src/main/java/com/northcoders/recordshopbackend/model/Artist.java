9d334863915c4f6078855d0b00f4ea9d2ab60009
Tawanda Chabva <tchabva@gmail.com>
2024-12-05 12:35:59 +0000

    feat: implemented the Hibernate ORM relationship for Arist
    OneToMany relation to albums mapped by the artist

--- src/main/java/com/northcoders/recordshopbackend/model/Artist.java
+++ src/main/java/com/northcoders/recordshopbackend/model/Artist.java
@@ -1,13 +1,9 @@
 package com.northcoders.recordshopbackend.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
+import java.util.Set;
 
 @Data
 @Entity
@@ -21,5 +17,6 @@ public class Artist {
     @Column(name = "artist_name")
     private String artistName;
 
-    private List<Album> albums;
+    @OneToMany(mappedBy = "artist")
+    private Set<Album> albums;
 }
