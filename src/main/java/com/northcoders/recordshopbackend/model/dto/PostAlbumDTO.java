ab707e17a73733d0d0c9bd98241fdf377964bfff
Tawanda Chabva <tchabva@gmail.com>
2024-12-05 11:54:18 +0000

    fix: added Genre enum attribute to PostAlbumDTO record

--- src/main/java/com/northcoders/recordshopbackend/model/dto/PostAlbumDTO.java
+++ src/main/java/com/northcoders/recordshopbackend/model/dto/PostAlbumDTO.java
@@ -1,6 +1,6 @@
 package com.northcoders.recordshopbackend.model.dto;
 
-import lombok.Data;
+import com.northcoders.recordshopbackend.model.Genre;
 
 // The data transfer object for creating new albumn entries
-public record PostAlbumDTO(String title, String artist, String releaseDate, int stock) {}
+public record PostAlbumDTO(String title, String artist, Genre genre, String releaseDate, int stock) {}
