004828ef07bc5ec479c1562d95133ba443f8de6a
Tawanda Chabva <tchabva@gmail.com>
2024-12-05 16:55:23 +0000

    feat: added methods for creating and converting from the Album DTOs in the AlbumService

--- src/main/java/com/northcoders/recordshopbackend/service/AlbumService.java
+++ src/main/java/com/northcoders/recordshopbackend/service/AlbumService.java
@@ -2,12 +2,16 @@ package com.northcoders.recordshopbackend.service;
 
 
 import com.northcoders.recordshopbackend.model.Album;
+import com.northcoders.recordshopbackend.model.dto.GetAlbumDTO;
+import com.northcoders.recordshopbackend.model.dto.PostAlbumDTO;
 
 import java.util.List;
 
 public interface AlbumService {
     List<Album> getAllAlbums();
     Album getAlbumById(Long albumId);
+    GetAlbumDTO createAlbumDTO (Album album);
+    Album createAlbum(PostAlbumDTO postAlbumDTO);
     Album addNewAlbum(Album album);
     Album updateAlbumStockById(Long albumId);
     String deleteAlbumById(Long albumId);
