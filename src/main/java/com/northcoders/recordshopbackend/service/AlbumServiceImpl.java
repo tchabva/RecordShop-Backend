7e4b91b919492bdef050aabea2fa5e239c6159e1
Tawanda Chabva <tchabva@gmail.com>
2024-12-05 15:35:11 +0000

    feat: implemented getAllAlbums method in AlbumServiceImpl

--- src/main/java/com/northcoders/recordshopbackend/service/AlbumServiceImpl.java
+++ src/main/java/com/northcoders/recordshopbackend/service/AlbumServiceImpl.java
@@ -1,13 +1,25 @@
 package com.northcoders.recordshopbackend.service;
 
 import com.northcoders.recordshopbackend.model.Album;
+import com.northcoders.recordshopbackend.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class AlbumServiceImpl implements AlbumService{
+
+    @Autowired
+    AlbumRepository albumRepository;
+
     @Override
     public List<Album> getAllAlbums() {
-        return List.of();
+        List<Album> albums = new ArrayList<>();
+        albumRepository.findAll().forEach(albums::add);
+
+        return albums;
     }
 
     @Override
