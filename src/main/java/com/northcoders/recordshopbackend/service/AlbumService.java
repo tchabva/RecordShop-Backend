8ec2b9e85049aae34a038ec2e90a6225586e1263
Tawanda Chabva <tchabva@gmail.com>
2024-12-05 14:30:04 +0000

    feat: added methods to AlbumService interface

--- src/main/java/com/northcoders/recordshopbackend/service/AlbumService.java
+++ src/main/java/com/northcoders/recordshopbackend/service/AlbumService.java
@@ -1,6 +1,14 @@
 package com.northcoders.recordshopbackend.service;
 
 
+import com.northcoders.recordshopbackend.model.Album;
+
+import java.util.List;
 
 public interface AlbumService {
+    List<Album> getAllAlbums();
+    Album getAlbumById(Long albumId);
+    Album addNewAlbum(Album album);
+    Album updateAlbumStockById(Long albumId);
+    String deleteAlbumById(Long albumId);
 }
