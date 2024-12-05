2c39bc5d4500d94be9c84df2e1abe08f86d8da90
Tawanda Chabva <tchabva@gmail.com>
2024-12-05 11:21:47 +0000

    Initial commit

---
+++ src/main/java/com/northcoders/recordshopbackend/NorthcodersRecordShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.recordshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthcodersRecordShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthcodersRecordShopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northcoders/recordshopbackend/NorthcodersRecordShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.recordshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthcodersRecordShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
