f549714a1750aa0ea3e35aa1248b7bab5a847d48
Tawanda Chabva <tchabva@gmail.com>
2024-12-06 10:40:13 +0000

    feat: created ArtistServiceImpl and implemented ArtistService and its methods

---
+++ src/main/java/com/northcoders/recordshopbackend/service/ArtistServiceImpl.java
@@ -0,0 +1,22 @@
+package com.northcoders.recordshopbackend.service;
+
+import com.northcoders.recordshopbackend.model.Artist;
+
+import java.util.List;
+
+public class ArtistServiceImpl implements ArtistService{
+    @Override
+    public List<Artist> getAllArtists() {
+        return List.of();
+    }
+
+    @Override
+    public Artist addNewArtist(String artistName) {
+        return null;
+    }
+
+    @Override
+    public Artist getOrAddNewArist(String artistName) {
+        return null;
+    }
+}
