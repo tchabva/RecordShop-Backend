2648e146056d9d5bd9023f42b99dba9fa1064a2a
Tawanda Chabva <tchabva@gmail.com>
2024-12-06 16:44:05 +0000

    feat: added PostMapping for the add new album to DB

--- src/main/java/com/northcoders/recordshopbackend/controllers/AlbumController.java
+++ src/main/java/com/northcoders/recordshopbackend/controllers/AlbumController.java
@@ -5,9 +5,7 @@ import com.northcoders.recordshopbackend.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,10 @@ public class AlbumController {
     @GetMapping
     public ResponseEntity<List<AlbumDTO>> getAllInStockAlbums(){
         return new ResponseEntity<>(albumService.getAllInStockAlbumDTOs(), HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<AlbumDTO> postAddNewAlbum(@RequestBody AlbumDTO albumDTO){
+        return new ResponseEntity<>(albumService.postNewAlbum(albumDTO), HttpStatus.CREATED);
     }
 }
