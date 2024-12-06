5b64b27baa800ed69b89e8df30514ea9747727aa
Tawanda Chabva <tchabva@gmail.com>
2024-12-06 10:36:30 +0000

    feat: implemented logic for methods in StockServiceImpl

--- src/main/java/com/northcoders/recordshopbackend/service/StockServiceImpl.java
+++ src/main/java/com/northcoders/recordshopbackend/service/StockServiceImpl.java
@@ -1,18 +1,32 @@
 package com.northcoders.recordshopbackend.service;
 
+import com.northcoders.recordshopbackend.exception.ItemNotFoundException;
 import com.northcoders.recordshopbackend.model.Stock;
+import com.northcoders.recordshopbackend.repository.StockRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StockServiceImpl implements StockService {
 
+    @Autowired
+    private StockRepository stockRepository;
+
     @Override
     public Stock addNewStock(int quantity) {
-        return null;
+        return stockRepository.save(
+                Stock.builder()
+                        .quantityInStock(quantity)
+                        .build()
+        );
     }
 
     @Override
     public Stock getStockById(Long stockId) {
-        return null;
+        if(stockRepository.findById(stockId).isPresent()){
+            return stockRepository.findById(stockId).get();
+        }else {
+         throw new ItemNotFoundException(String.format("Stock with the id '%s' cannot be found", stockId));
+        }
     }
 }
