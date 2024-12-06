ec2f17c39878f93c743e720b1973957e78afe64b
Tawanda Chabva <tchabva@gmail.com>
2024-12-06 10:15:22 +0000

    feat: created the StockServiceImpl with implements the StockService methods

---
+++ src/main/java/com/northcoders/recordshopbackend/service/StockServiceImpl.java
@@ -0,0 +1,18 @@
+package com.northcoders.recordshopbackend.service;
+
+import com.northcoders.recordshopbackend.model.Stock;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockServiceImpl implements StockService {
+
+    @Override
+    public Stock addNewStock(int quantity) {
+        return null;
+    }
+
+    @Override
+    public Stock getStockById(Long stockId) {
+        return null;
+    }
+}
