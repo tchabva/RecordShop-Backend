35bad9f270ecf82e1c51f4fbd38fda4aa04eecc8
Tawanda Chabva <tchabva@gmail.com>
2024-12-06 10:03:20 +0000

    feat: created StockService interface with addNewStock and getStockById methods

---
+++ src/main/java/com/northcoders/recordshopbackend/service/StockService.java
@@ -0,0 +1,8 @@
+package com.northcoders.recordshopbackend.service;
+
+import com.northcoders.recordshopbackend.model.Stock;
+
+public interface StockService {
+    Stock addNewStock(int quantity);
+    Stock getStockById(Long stockId);
+}
