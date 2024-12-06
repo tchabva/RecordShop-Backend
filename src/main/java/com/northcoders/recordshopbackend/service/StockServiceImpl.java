fffe0f56eccf76a3e5368ddde0ff4c91c8179455
Tawanda Chabva <tchabva@gmail.com>
2024-12-06 14:09:14 +0000

    feat: implemented saveUpdatedStock in StockServiceImpl

--- src/main/java/com/northcoders/recordshopbackend/service/StockServiceImpl.java
+++ src/main/java/com/northcoders/recordshopbackend/service/StockServiceImpl.java
@@ -32,6 +32,6 @@ public class StockServiceImpl implements StockService {
 
     @Override
     public Stock savedUpdatedStock(Stock updatedStock) {
-        return null;
+        return stockRepository.save(updatedStock);
     }
 }
