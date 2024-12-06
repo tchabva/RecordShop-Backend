6d743d0839d5e37ea55c014abc44870739ed1a8e
Tawanda Chabva <tchabva@gmail.com>
2024-12-06 11:20:55 +0000

    feat: implemented getAllArtists in ArtistServiceImpl

--- src/main/java/com/northcoders/recordshopbackend/service/ArtistServiceImpl.java
+++ src/main/java/com/northcoders/recordshopbackend/service/ArtistServiceImpl.java
@@ -1,13 +1,24 @@
 package com.northcoders.recordshopbackend.service;
 
 import com.northcoders.recordshopbackend.model.Artist;
+import com.northcoders.recordshopbackend.repository.ArtistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class ArtistServiceImpl implements ArtistService{
+
+    @Autowired
+    private ArtistRepository artistRepository;
+
     @Override
     public List<Artist> getAllArtists() {
-        return List.of();
+        List<Artist> artists = new ArrayList<>();
+        artistRepository.findAll().forEach(artists::add);
+        return artists;
     }
 
     @Override
