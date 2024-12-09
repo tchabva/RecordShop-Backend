cee44533840dda0c21bbd0d22763c2ae91ca5d8c
Tawanda Chabva <tchabva@gmail.com>
2024-12-09 22:44:03 +0000

    feat: updated AblumCache with map methods

--- src/main/java/com/northcoders/recordshopbackend/service/AlbumCache.java
+++ src/main/java/com/northcoders/recordshopbackend/service/AlbumCache.java
@@ -12,4 +12,16 @@ public class AlbumCache {
     private final HashMap<Long, Album> albumCache = new HashMap<>();
     private boolean isValid;
 
+    void put(Long albumId, Album album){
+        this.albumCache.put(albumId, album);
+    }
+
+    boolean containsKey(Long albumId){
+        return this.albumCache.containsKey(albumId);
+    }
+
+    Album get(Long albumId){
+        return this.albumCache.get(albumId);
+    }
+
 }
