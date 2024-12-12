94f89eed87c54b6bdd1b1dc056d3bbcf59acd0e0
Tawanda Chabva <tchabva@gmail.com>
2024-12-12 22:51:40 +0000

    refactor: changed from a Genre enum to an Entity

---
+++ src/main/java/com/northcoders/recordshopbackend/model/Genre.java
@@ -0,0 +1,28 @@
+package com.northcoders.recordshopbackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "genres")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String genre;
+
+    @OneToMany(mappedBy = "genre")
+    private Set<Album> albums;
+}
