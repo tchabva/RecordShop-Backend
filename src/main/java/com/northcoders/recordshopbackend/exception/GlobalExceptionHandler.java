6652d1a6b8234a98c8ea981742f053358e796e07
Tawanda Chabva <tchabva@gmail.com>
2024-12-14 00:25:03 +0000

    refator: update GlobalExceptionHandler to return a Map instead of a single string

--- src/main/java/com/northcoders/recordshopbackend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/northcoders/recordshopbackend/exception/GlobalExceptionHandler.java
@@ -2,12 +2,13 @@ package com.northcoders.recordshopbackend.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.Arrays;
+import java.util.*;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,8 +21,17 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+
+        Map<String, String> errors = new HashMap<>();
+
+        e.getBindingResult().getAllErrors().forEach(objectError ->{
+            String fieldName = ((FieldError) objectError).getField();
+            String errorMessage = objectError.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
         return new ResponseEntity<>(
-                Arrays.stream(e.getDetailMessageArguments()).toList().getLast(),
+               errors,
                 HttpStatus.BAD_REQUEST
         );
     }
