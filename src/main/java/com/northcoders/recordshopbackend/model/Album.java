a570c463115f69de81f2375d8fd8b6d4cc03f739
Tawanda Chabva <tchabva@gmail.com>
2024-12-15 00:20:14 +0000

    style: remove unused import in Album model

--- src/main/java/com/northcoders/recordshopbackend/model/Album.java
+++ src/main/java/com/northcoders/recordshopbackend/model/Album.java
@@ -1,6 +1,5 @@
 package com.northcoders.recordshopbackend.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
