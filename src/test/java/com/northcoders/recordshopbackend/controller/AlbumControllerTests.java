563488fb3f720ed8e7413e5ba44a56c599cb6a92
Tawanda Chabva <tchabva@gmail.com>
2024-12-15 14:14:05 +0000

    refactor: changed controllers package name to controller

--- src/test/java/com/northcoders/recordshopbackend/controller/AlbumControllerTests.java
+++ src/test/java/com/northcoders/recordshopbackend/controller/AlbumControllerTests.java
@@ -1,4 +1,4 @@
-package com.northcoders.recordshopbackend.controllers;
+package com.northcoders.recordshopbackend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.northcoders.recordshopbackend.dto.AlbumDTO;
