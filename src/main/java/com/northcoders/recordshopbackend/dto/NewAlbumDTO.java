a8cd5881a128f348efe7cf0e322a62740f06918a
Tawanda Chabva <tchabva@gmail.com>
2024-12-15 00:26:03 +0000

    refactor : changed annotation from Min to PostiveOrZero

--- src/main/java/com/northcoders/recordshopbackend/dto/NewAlbumDTO.java
+++ src/main/java/com/northcoders/recordshopbackend/dto/NewAlbumDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -33,10 +34,10 @@ public class NewAlbumDTO {
     private Date releaseDate;
 
     @NotNull(message = "A stock quantity is mandatory")
-    @Min(0)
+    @PositiveOrZero(message = "A stock quantity is mandatory")
     private Integer stock;
 
     @NotNull(message = "A price is mandatory")
-    @Min(0)
+    @PositiveOrZero(message = "A stock quantity is mandatory")
     private Double price;
 }
