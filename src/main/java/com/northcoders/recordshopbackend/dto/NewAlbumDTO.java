75fa9def22eb9c9516a92ee3a133c8c418024bb0
Tawanda Chabva <tchabva@gmail.com>
2024-12-15 00:26:45 +0000

    style: remove unused import

--- src/main/java/com/northcoders/recordshopbackend/dto/NewAlbumDTO.java
+++ src/main/java/com/northcoders/recordshopbackend/dto/NewAlbumDTO.java
@@ -1,7 +1,6 @@
 package com.northcoders.recordshopbackend.dto;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
