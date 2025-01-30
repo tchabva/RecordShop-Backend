3b74b34f788c2ddbde250e02b1035625190b6d1a
Tawanda Chabva <tchabva@gmail.com>
2025-01-30 00:42:36 +0000

    feat: add List of AlbumDTOs to ArtistDTO

--- src/main/java/com/northcoders/recordshopbackend/dto/ArtistDTO.java
+++ src/main/java/com/northcoders/recordshopbackend/dto/ArtistDTO.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @Builder
 public class ArtistDTO {
     private Long id;
     private String artistName;
+    private List<AlbumDTO> albums;
 }
