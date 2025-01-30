d4f261de6903e755f638f41f9b3a03e7570ce62d
Tawanda Chabva <tchabva@gmail.com>
2025-01-30 14:08:16 +0000

    refactor: removed unneeded method

--- src/main/java/com/northcoders/recordshopbackend/repository/ArtistRepository.java
+++ src/main/java/com/northcoders/recordshopbackend/repository/ArtistRepository.java
@@ -2,8 +2,6 @@ package com.northcoders.recordshopbackend.repository;
 
 import com.northcoders.recordshopbackend.model.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,7 +9,4 @@ import java.util.Optional;
 @Repository
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
     Optional<Artist> findByArtistName(String artistName);
-
-    @Query("SELECT a FROM Artist a LEFT JOIN FETCH a.albums WHERE a.id = :artistId")
-    Optional<Artist> findByIdWithAlbums(@Param("artistId") Long artistId);
 }
