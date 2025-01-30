9aad39a90183480c319b17fdb86fce758552938c
Tawanda Chabva <tchabva@gmail.com>
2025-01-30 00:40:13 +0000

    fix: changed @Data lombok annotation to @Getter and @Setter to remove circular reference

--- src/main/java/com/northcoders/recordshopbackend/model/Artist.java
+++ src/main/java/com/northcoders/recordshopbackend/model/Artist.java
@@ -1,17 +1,15 @@
 package com.northcoders.recordshopbackend.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
 @Entity
 @Table(name = "artists")
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
