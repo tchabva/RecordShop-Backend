c67ec7c4c827b50690e763e985b7c85dfff255e9
Tawanda Chabva <tchabva@gmail.com>
2025-01-30 17:02:35 +0000

    refactor: changed @Data lombok annotation to @Getter & @Setter

--- src/main/java/com/northcoders/recordshopbackend/model/Genre.java
+++ src/main/java/com/northcoders/recordshopbackend/model/Genre.java
@@ -1,16 +1,14 @@
 package com.northcoders.recordshopbackend.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Set;
 
 @Entity
 @Table(name = "genres")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
